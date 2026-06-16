cc4b26aeff3afcf2947ec5497dec0106fac6e67e
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-16 14:18:08 +0800

    Update project

---
+++ backend/src/main/java/com/nestcart/NestCartApplication.java
@@ -0,0 +1,13 @@
+package com.nestcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class NestCartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NestCartApplication.class, args);
+    }
+}
